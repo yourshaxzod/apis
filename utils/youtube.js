ce9e59bfca11c74d87def6692211a1f6fc9d04e5
Shaxzod :) <167005121+yourshaxzod@users.noreply.github.com>
2024-08-01 21:24:28 +0500

    Update youtube.js

--- utils/youtube.js
+++ utils/youtube.js
@@ -4,47 +4,36 @@ let getYoutube = async (url) => {
   try {
     const info = await ytdl.getInfo(url);
 
-    // Faqat video formatlarini filtrlash
-    const videoFormats = info.formats.filter(format => format.mimeType.startsWith('video/mp4'));
+    // 'avc1' bilan boshlanuvchi formatlarni filtrlash
+    const videoFormats = info.formats.filter(format => 
+      format.mimeType.startsWith('video/mp4') && 
+      format.mimeType.includes('avc1')
+    );
 
-    // Faqat audio formatlarini filtrlash
-    const audioFormats = info.formats.filter(format => format.mimeType.startsWith('audio/mp4'));
-
-    // Yaxshi sifatdagi video formatini tanlash
-    const bestVideoFormat = videoFormats.reduce((best, format) => {
-      if (!best || (format.qualityLabel && format.qualityLabel > best.qualityLabel)) {
-        return format;
-      }
-      return best;
-    }, null);
-
-    // Yaxshi sifatdagi audio formatini tanlash
-    const bestAudioFormat = audioFormats.reduce((best, format) => {
-      if (!best || (format.audioBitrate && format.audioBitrate > best.audioBitrate)) {
-        return format;
-      }
-      return best;
-    }, null);
+    const audioFormats = info.formats.filter(format => 
+      format.mimeType.startsWith('audio/mp4') && 
+      format.mimeType.includes('avc1')
+    );
 
     // Formatlarni saqlash
     const formats = [
-      bestVideoFormat ? {
-        itag: bestVideoFormat.itag,
-        mimeType: bestVideoFormat.mimeType,
-        quality: bestVideoFormat.qualityLabel || bestVideoFormat.quality,
-        audio: bestVideoFormat.audioBitrate || null,
-        type: bestVideoFormat.container || null,
-        url: bestVideoFormat.url
-      } : null,
-      bestAudioFormat ? {
-        itag: bestAudioFormat.itag,
-        mimeType: bestAudioFormat.mimeType,
-        quality: bestAudioFormat.qualityLabel || bestAudioFormat.quality,
-        audio: bestAudioFormat.audioBitrate || null,
-        type: bestAudioFormat.container || null,
-        url: bestAudioFormat.url
-      } : null
-    ].filter(format => format !== null); // Null formatlarni olib tashlash
+      ...videoFormats.map(format => ({
+        itag: format.itag,
+        mimeType: format.mimeType,
+        quality: format.qualityLabel || format.quality,
+        audio: format.audioBitrate || null,
+        type: format.container || null,
+        url: format.url
+      })),
+      ...audioFormats.map(format => ({
+        itag: format.itag,
+        mimeType: format.mimeType,
+        quality: format.qualityLabel || format.quality,
+        audio: format.audioBitrate || null,
+        type: format.container || null,
+        url: format.url
+      }))
+    ];
 
     return {
       ok: true,
