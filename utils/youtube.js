f1d6c6b097b5282fd60bc84b20988638cfbf8351
Shaxzod :) <167005121+yourshaxzod@users.noreply.github.com>
2024-08-02 17:23:29 +0500

    Update youtube.js

--- utils/youtube.js
+++ utils/youtube.js
@@ -1,61 +1,74 @@
-const ytdl = require('@distube/ytdl-core');
+const ytdl = require('@distube/ytdl-core')
 
 let getYoutube = async (url) => {
-  try {
-    const info = await ytdl.getInfo(url);
+    try {
+        const info = await ytdl.getInfo(url)
 
-    // 'avc1' bilan boshlanuvchi formatlarni filtrlash
-    const videoFormats = info.formats.filter(format => 
-      format.mimeType.startsWith('video/mp4') && 
-      format.mimeType.includes('av01')
-    );
+        const formats = info.formats.filter(format =>
+            format.mimeType.includes('video/mp4') || format.mimeType.includes('audio/mp4')
+        );
 
-    const audioFormats = info.formats.filter(format => 
-      format.mimeType.startsWith('audio/mp4') && 
-      format.mimeType.includes('mp4a')
-    );
+        const uniqueFormats = {}
 
-    // Formatlarni saqlash
-    const formats = [
-      ...videoFormats.map(format => ({
-        itag: format.itag,
-        mimeType: format.mimeType,
-        quality: format.qualityLabel || format.quality,
-        audio: format.audioBitrate || null,
-        type: format.container || null,
-        url: format.url
-      })),
-      ...audioFormats.map(format => ({
-        itag: format.itag,
-        mimeType: format.mimeType,
-        quality: format.qualityLabel || format.quality,
-        audio: format.audioBitrate || null,
-        type: format.container || null,
-        url: format.url
-      }))
-    ];
+        formats.forEach(format => {
+            const qualityKey = format.qualityLabel || format.audioBitrate
+            if (!uniqueFormats[qualityKey]) {
+                uniqueFormats[qualityKey] = format
+            }
+        })
 
-    return {
-      ok: true,
-      info: {
-        id: info.videoDetails.videoId,
-        title: info.videoDetails.title,
-        caption: info.videoDetails.description,
-        duration: info.videoDetails.lengthSeconds,
-        views: info.viewCount,
-        author: info.videoDetails.author.name,
-        username: info.videoDetails.author.user,
-        thumbnail: info.videoDetails.thumbnails?.[info.videoDetails.thumbnails.length - 1]?.url || null,
-        formats: formats
-      }
-    };
-  } catch (error) {
-    console.log(error);
-    return {
-      ok: false,
-      msg: "Error fetching data.",
-    };
-  }
+        const sortedFormats = Object.values(uniqueFormats).sort((a, b) => {
+            const qualityA = a.qualityLabel ? parseInt(a.qualityLabel) : a.audioBitrate
+            const qualityB = b.qualityLabel ? parseInt(b.qualityLabel) : b.audioBitrate
+            return qualityA - qualityB
+        });
+
+        const videoFormats = sortedFormats.filter(format => format.mimeType.includes('video/mp4'))
+        const audioFormats = sortedFormats.filter(format => format.mimeType.includes('audio/mp4'))
+
+        // Formatlarni saqlash
+        const formatedFormats = [
+            ...videoFormats.map(format => ({
+                itag: format.itag,
+                mimeType: format.mimeType,
+                quality: format.qualityLabel || format.quality,
+                audio: format.audioBitrate || null,
+                type: format.container || null,
+                contentLength: format.contentLength,
+                url: format.url
+            })),
+            ...audioFormats.map(format => ({
+                itag: format.itag,
+                mimeType: format.mimeType,
+                quality: format.qualityLabel || format.quality,
+                audio: format.audioBitrate || null,
+                type: format.container || null,
+                contentLength: format.contentLength,
+                url: format.url
+            }))
+        ];
+
+        return {
+            ok: true,
+            info: {
+                id: info.videoDetails.videoId,
+                title: info.videoDetails.title,
+                caption: info.videoDetails.description,
+                duration: info.videoDetails.lengthSeconds,
+                views: info.viewCount,
+                author: info.videoDetails.author.name,
+                username: info.videoDetails.author.user,
+                thumbnail: info.videoDetails.thumbnails?.[info.videoDetails.thumbnails.length - 1]?.url || null,
+                formats: formatedFormats
+            }
+        };
+    } catch (error) {
+        console.log(error);
+        return {
+            ok: false,
+            msg: "Error fetching data.",
+        };
+    }
 };
 
-module.exports = { getYoutube };
+module.exports = { getYoutube }
