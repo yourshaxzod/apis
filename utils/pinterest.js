946eeb02050a0c07612c0e0df5a3312792231568
yourshaxzod <yourshaxzod@gmail.com>
2024-08-09 22:00:38 +0500

    fix

--- utils/pinterest.js
+++ utils/pinterest.js
@@ -6,15 +6,13 @@ let getPinterest = async (url) => {
             'accept': '*/*',
             'accept-encoding': 'gzip, deflate, br, zstd',
             'accept-language': 'en,ru;q=0.9,ru-RU;q=0.8,uz;q=0.7',
+            //'cookie': '_ga=GA1.1.626023615.1723220771; ci_session=7vab7ps8m2gpfs9r3qi0ldisspr47ul3; _ga_E7SFLTHFL8=GS1.1.1723220771.1.1.1723222588.0.0.0',
             'referer': 'https://pinterestdownloader.io/',
-            'sec-fetch-dest': 'empty',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-site': 'same-origin',
-            'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1'
+            'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1',
         }
 
         url = `https://pinterestdownloader.io/frontendService/DownloaderService?url=${url}`
-        const response = await axios.get(url, { headers})
+        const response = await axios.get(url, { headers })
 
         let info = response.data
 
